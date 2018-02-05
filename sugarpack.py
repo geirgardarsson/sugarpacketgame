a4329c108aac7cc2f6449fdf5b94b3059e5f6096
geirgardarsson <geg42@hi.is>
2018-02-05 14:58:59 +0000

    cleanup

--- sugarpack.py
+++ sugarpack.py
@@ -6,14 +6,12 @@ import datetime
 
 # boxchars: ╋ ━┃┣ ┫┳ ┻ ┛🡺🡻
 
-test = ['a', 'b', 'c', '1', '2', '3']
-player1 = 'abc'
-player2 = '123'
+player1 = ['a', 'b', 'c']
+player2 = ['1', '2', '3']
 darrow = '🡻'
 rarrow = '🡺'
 
-# goes_first = random.randint(0,1)
-goes_first = 0
+active_player = random.randint(0,1)
 game_over = False
 
 # Boardstate
@@ -24,24 +22,42 @@ bs = ([[' ', 'a', 'b', 'c', ' '],
        [' ', ' ', ' ', ' ', ' ']])
 
 
-row0 = ' {0} ┃ {1}   {2}   {3} ┃ {4}\n━━━╋━━━┳━━━┳━━━┫\n'.format(*bs[0])
-row1 = ' {0} ┃ {1} ┃ {2} ┃ {3} ┃ {4}\n   ┣━━━╋━━━╋━━━┫\n'.format(*bs[1])
-row2 = ' {0} ┃ {1} ┃ {2} ┃ {3} ┃ {4}\n   ┣━━━╋━━━╋━━━┫\n'.format(*bs[2])
-row3 = ' {0} ┃ {1} ┃ {2} ┃ {3} ┃ {4}\n━━━┻━━━┻━━━┻━━━┛\n'.format(*bs[3])
-row4 = ' {0}   {1}   {2}   {3}   {4}\n'.format(*bs[4])
+def refresh(bs):
 
-grid = row0 + row1 + row2 + row3 + row4
+    row0 = ' {0} ┃ {1}   {2}   {3} ┃ {4}\n━━━╋━━━┳━━━┳━━━┫\n'.format(*bs[0])
+    row1 = ' {0} ┃ {1} ┃ {2} ┃ {3} ┃ {4}\n   ┣━━━╋━━━╋━━━┫\n'.format(*bs[1])
+    row2 = ' {0} ┃ {1} ┃ {2} ┃ {3} ┃ {4}\n   ┣━━━╋━━━╋━━━┫\n'.format(*bs[2])
+    row3 = ' {0} ┃ {1} ┃ {2} ┃ {3} ┃ {4}\n━━━┻━━━┻━━━┻━━━┛\n'.format(*bs[3])
+    row4 = ' {0}   {1}   {2}   {3}   {4}\n'.format(*bs[4])
 
-def render():
+    grid = row0 + row1 + row2 + row3 + row4
+    return grid
+
+
+def render(grid):
     subprocess.call('clear')
     print(grid)
+    print('Player {0}\'s turn,'.format(active_player + 1))
 
 
-def main():
-    player = goes_first
+def main(grid, player):
     # while (not game_over):
-    render()
+    render(grid)
     main_phase(player)
+
+
+def main_phase(player):
+
+    player_chars = player1 if player == 0 else player2
+
+    # if (player == 1):
+    while True:
+        piece_to_move = input('Select a piece to move (legal: {0} {1} {2}): '.format(*player_chars))
+        if any(piece_to_move in i for i in player_chars):
+            break
+        
+    move_piece(player, piece_to_move)
+    player = (player + 1) % 2
 
 
 def move_piece(player, piece):
@@ -59,25 +75,18 @@ def move_piece(player, piece):
     
     if (player == 0):
         tmp = piece
-
+        bs[col][row] = darrow
+        bs[col+1][row] = tmp
 
     else:
         tmp = piece
+        bs[col][row] = rarrow
+        bs[col][row+1] = tmp
+
+    newgrid = refresh(bs)
+    render(newgrid)
 
 
-    print(player, piece)
-
-
-def main_phase(player):
-
-    # if (player == 1):
-    piece_to_move = input('Select a piece to move: ')
-    move_piece(player, piece_to_move)
-
-
-
-    player = (player + 1) % 2
-
-
-main()
+grid = refresh(bs)
+main(grid, active_player)
 
